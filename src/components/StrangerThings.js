e2286fdfa4b8a661a58fb371fc94eaa4aafedd93
pedrohcalado <pedrocalado22@gmail.com>
2020-12-10 16:15:58 -0300

    Adiciona variaveis de ambiente

--- src/components/StrangerThings.js
+++ src/components/StrangerThings.js
@@ -5,14 +5,15 @@ const getRealityClass = (hereIsTheUpsideDownWorld) => (
   hereIsTheUpsideDownWorld ? 'upside-down' : 'stranger-things'
 );
 
+// console.log(process.env);
 const strangerThingsConfig = {
-  url: 'http://localhost:3002',
-  timeout: 30000,
+  url: process.env.REACT_APP_HAWKINS_URL,
+  timeout: process.env.REACT_APP_HAWKINS_TIMEOUT,
 };
 
 const upsideDownConfig = {
-  url: 'http://localhost:3003',
-  timeout: 30000,
+  url: process.env.REACT_APP_UPSIDEDOWN_URL,
+  timeout: process.env.REACT_APP_UPSIDEDOWN_TIMEOUT,
 };
 
 const charactersService = new CharactersService(strangerThingsConfig);
