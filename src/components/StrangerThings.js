d88f6175fd1831021eeb082bd6d969ad8a0e3454
Juliette Beaudet <juliette.beaudet@sciencespo.fr>
2020-12-10 19:28:30 -0300

    trying with dev_mode env

--- src/components/StrangerThings.js
+++ src/components/StrangerThings.js
@@ -106,9 +106,10 @@ class StrangerThings extends React.Component {
       <div className={`reality ${getRealityClass(hereIsTheUpsideDownWorld)}`}>
         <div className="content strangerfy">
           <div>
-            {process.env.ENV_MODE === 'Em desenvolvimento' && <button type="button">Em desenvolvimento</button>}
+            {DEV_MODE && <h2>Em desenvolvimento</h2>}
+            {/* {process.env.ENV_MODE === 'Em desenvolvimento' && <button type="button">Em desenvolvimento</button>}
             <button type="button"> Env_Mode: {process.env.ENV_MODE}</button>
-            <button type="button"> Dev_Mode: {process.env.DEV_MODE}</button>
+            <button type="button"> Dev_Mode: {process.env.DEV_MODE}</button> */}
           </div>
           <div className="change-reality">
             <button type="button" onClick={this.changeRealityClick}>
