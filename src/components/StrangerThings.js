86a5fbd61f92a077d616d1a543b4aee8a293958d
hleoc <informig@gmail.com>
2020-12-10 22:28:59 -0300

    Requisito 8 - Corrigindo erros do eslint

--- src/components/StrangerThings.js
+++ src/components/StrangerThings.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import CharactersService from '../services/charactersAPI';
+
 require('dotenv').config();
 
 const getRealityClass = (hereIsTheUpsideDownWorld) => (
@@ -7,15 +8,15 @@ const getRealityClass = (hereIsTheUpsideDownWorld) => (
 );
 
 const strangerThingsConfig = {
-  url: 'http://localhost:3002',
-  timeout: 30000,
+  // url: 'http://localhost:3002',
+  // timeout: 30000,
   url: process.env.REACT_APP_HAWKINS_URL,
   timeout: process.env.REACT_APP_HAWKINS_TIMEOUT,
 };
 
 const upsideDownConfig = {
-  url: 'http://localhost:3003',
-  timeout: 30000,
+  // url: 'http://localhost:3003',
+  // timeout: 30000,
   url: process.env.REACT_APP_UPSIDEDOWN_URL,
   timeout: process.env.REACT_APP_UPSIDEDOWN_TIMEOUT,
 };
