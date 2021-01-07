cac19fd6632a080bfe889a3c53204a1bc66f44dc
rfreitasbatista <rfreitasbatista@gmail.com>
2021-01-06 21:31:20 -0300

    LINT

--- src/components/StrangerThings.js
+++ src/components/StrangerThings.js
@@ -12,6 +12,8 @@ const strangerThingsConfig = {
   timeout: process.env.REACT_APP_HAWKINS_TIMEOUT,
 };
 
+console.log(strangerThingsConfig.url);
+
 const upsideDownConfig = {
   url: process.env.REACT_APP_UPSIDEDOWN_URL,
   timeout: process.env.REACT_APP_UPSIDEDOWN_TIMEOUT,
