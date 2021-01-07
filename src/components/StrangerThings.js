9de66a7ea8ef69107936fcffb45a8d6596c4fdec
samueap <samueap@gmail.com>
2021-01-07 11:39:50 -0300

    deploy

--- src/components/StrangerThings.js
+++ src/components/StrangerThings.js
@@ -12,10 +12,14 @@ const strangerThingsConfig = {
   timeout: process.env.REACT_APP_HAWKINS_TIMEOUT,
 };
 
+// console.log(strangerThingsConfig.url);
+
 const upsideDownConfig = {
   url: process.env.REACT_APP_UPSIDEDOWN_URL,
   timeout: process.env.REACT_APP_UPSIDEDOWN_TIMEOUT,
 };
+
+// console.log(upsideDownConfig.url);
 
 const charactersService = new CharactersService(strangerThingsConfig);
 const charactersUpsideDownService = new CharactersService(upsideDownConfig);
