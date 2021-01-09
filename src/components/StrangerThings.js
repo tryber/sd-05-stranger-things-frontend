a0ff5f2aa13a69b703d672d6a37adcb6a0094be3
Jorge Lucas <jorge.lucas2264@gmail.com>
2021-01-09 14:34:20 -0300

    testando novamente

--- src/components/StrangerThings.js
+++ src/components/StrangerThings.js
@@ -114,7 +114,6 @@ class StrangerThings extends React.Component {
       >
         <div className="content strangerfy">
           {(process.env.AMBIENTE === 'homolog') && (<div>Em desenvolvimento</div>)}
-          Em Desenvolvimento
           <div className="change-reality">
             <button type="button" onClick={ this.changeRealityClick }>
               {' '}
