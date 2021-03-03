3c4079718a918e0edc9b68e50e821b8cf1fb1c1f
saturnian99 <saturnian.dsg@gmail.com>
2021-03-03 01:13:44 -0300

    Fixing 'development' tag

--- src/components/StrangerThings.js
+++ src/components/StrangerThings.js
@@ -112,8 +112,8 @@ class StrangerThings extends React.Component {
           hereIsTheUpsideDownWorld,
         )}` }
       >
+        <div>Em desenvolvimento</div>
         <div className="content strangerfy">
-          <div>Em desenvolvimento</div>
           <div className="change-reality">
             <button type="button" onClick={ this.changeRealityClick }>
               {' '}
