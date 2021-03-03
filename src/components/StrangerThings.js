af18b018c42725491e70f11363dde753487c2e2e
Hugo J. De Moro Jr <hugo.urano@gmail.com>
2021-03-03 01:34:39 -0300

    requisito 9

--- src/components/StrangerThings.js
+++ src/components/StrangerThings.js
@@ -13,8 +13,8 @@ const strangerThingsConfig = {
 };
 
 const upsideDownConfig = {
-  url: process.env.REACT_APP_UPSIDESOWN_URL,
-  timeout: process.env.REACT_APP_UPSIDESOWN_TIMEOUT,
+  url: process.env.REACT_APP_UPSIDEDOWN_URL,
+  timeout: process.env.REACT_APP_UPSIDEDOWN_TIMEOUT,
 };
 
 const charactersService = new CharactersService(strangerThingsConfig);
