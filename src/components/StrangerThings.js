7d5a4ae3088a0f07c873c7957d2cf50cc717d6c9
Hugo J. De Moro Jr <hugo.urano@gmail.com>
2021-03-03 00:58:04 -0300

    requisito 9

--- src/components/StrangerThings.js
+++ src/components/StrangerThings.js
@@ -8,12 +8,12 @@ const getRealityClass = (hereIsTheUpsideDownWorld) => (
 );
 
 const strangerThingsConfig = {
-  url: process.env.REACT_APP_HAWKINS_URL || 'http://localhost:3002',
+  url: process.env.REACT_APP_HAWKINS_URL,
   timeout: process.env.REACT_APP_HAWKINS_TIMEOUT,
 };
 
 const upsideDownConfig = {
-  url: process.env.REACT_APP_UPSIDESOWN_URL || 'http://localhost:3003',
+  url: process.env.REACT_APP_UPSIDESOWN_URL,
   timeout: process.env.REACT_APP_UPSIDESOWN_TIMEOUT,
 };
 
